ee84c715c9f60cbe39229f217ad87567590e7d78
Ansgar Nell <ansgar.nell@gmail.com>
2020-03-30 18:33:50 +0200

    Initial commit

---
+++ src/main/java/com/medium/HR/Tool/Backend/HrToolBackendApplication.java
@@ -0,0 +1,13 @@
+package com.medium.HR.Tool.Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrToolBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrToolBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/medium/HR/Tool/Backend/HrToolBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medium.HR.Tool.Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrToolBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
