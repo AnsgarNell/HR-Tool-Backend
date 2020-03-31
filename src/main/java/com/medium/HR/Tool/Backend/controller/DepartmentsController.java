f69430897e05e9d0fe5d40c41778004939e8f21e
Ansgar Nell <ansgar.nell@gmail.com>
2020-03-31 22:38:03 +0200

    Allow CrossOrigin from any source for now to test the Angular frontend.

--- src/main/java/com/medium/HR/Tool/Backend/controller/DepartmentsController.java
+++ src/main/java/com/medium/HR/Tool/Backend/controller/DepartmentsController.java
@@ -2,6 +2,7 @@ package com.medium.HR.Tool.Backend.controller;
 
 import com.medium.HR.Tool.Backend.model.services.DepartmentsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping(value = "/departments")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class DepartmentsController {
 
     @Autowired
