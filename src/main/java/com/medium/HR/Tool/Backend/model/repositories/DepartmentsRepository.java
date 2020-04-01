0ae5a228dbbd6f206fda0a65f76a53974b0e618b
Ansgar Nell <ansgar.nell@gmail.com>
2020-04-01 21:07:35 +0200

    Restored repository name to plural form.

--- src/main/java/com/medium/HR/Tool/Backend/model/repositories/DepartmentsRepository.java
+++ src/main/java/com/medium/HR/Tool/Backend/model/repositories/DepartmentsRepository.java
@@ -3,5 +3,5 @@ package com.medium.HR.Tool.Backend.model.repositories;
 import com.medium.HR.Tool.Backend.model.Department;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface DepartmentRepository extends JpaRepository<Department, String> {
+public interface DepartmentsRepository extends JpaRepository<Department, String> {
 }
