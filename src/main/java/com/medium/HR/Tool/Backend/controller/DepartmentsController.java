96b39b5d188b1b322cebf23dcd0d47ac151a949d
Ansgar Nell <ansgar.nell@gmail.com>
2020-04-02 17:13:06 +0200

    Added mapping to get department by id and implemented a custom response.

--- src/main/java/com/medium/HR/Tool/Backend/controller/DepartmentsController.java
+++ src/main/java/com/medium/HR/Tool/Backend/controller/DepartmentsController.java
@@ -1,12 +1,20 @@
 package com.medium.HR.Tool.Backend.controller;
 
-import com.medium.HR.Tool.Backend.model.Departments;
+import com.medium.HR.Tool.Backend.model.Department;
 import com.medium.HR.Tool.Backend.model.repositories.DepartmentsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.HttpServerErrorException;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "/departments")
@@ -26,7 +34,16 @@ public class DepartmentsController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Departments> getDepartmentById(@PathVariable String id) {
-        return ResponseEntity.of(departmentsRepository.findById(id));
+    public ResponseEntity<Map<String,Object>> getDepartmentById(@PathVariable String id) {
+        Optional<Department> optionalDepartment = departmentsRepository.findById(id);
+
+        if(optionalDepartment.isPresent()) {
+            Department department = optionalDepartment.get();
+            Map<String,Object> map = new HashMap<>();
+            map.put("Department", department);
+            map.put("Managers", department.getManagers());
+            return ResponseEntity.ok(map);
+        }
+        else throw new HttpServerErrorException(HttpStatus.NOT_FOUND);
     }
 }
