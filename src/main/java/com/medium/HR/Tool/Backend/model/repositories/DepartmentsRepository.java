6fe63014afd08ec1a81e1fb900ad9fb54ecd8320
Ansgar Nell <ansgar.nell@gmail.com>
2020-04-03 11:33:21 +0200

    Add find all function ordered by ascending department number.

--- src/main/java/com/medium/HR/Tool/Backend/model/repositories/DepartmentsRepository.java
+++ src/main/java/com/medium/HR/Tool/Backend/model/repositories/DepartmentsRepository.java
@@ -3,9 +3,12 @@ package com.medium.HR.Tool.Backend.model.repositories;
 import com.medium.HR.Tool.Backend.model.Department;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 /**
  * The DepartmentsRepository class contains the implementation of
  * the JPA Repository for the departments
  */
 public interface DepartmentsRepository extends JpaRepository<Department, String> {
+    List<Department> findAllByOrderByDeptNoAsc();
 }
