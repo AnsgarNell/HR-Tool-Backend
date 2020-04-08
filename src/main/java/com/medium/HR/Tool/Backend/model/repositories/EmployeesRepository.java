0e83769652f66abcd1bb377a72e18786b6ede69f
Ansgar Nell <ansgar.nell@gmail.com>
2020-04-08 17:10:03 +0200

    Added pageable findAll query.

--- src/main/java/com/medium/HR/Tool/Backend/model/repositories/EmployeesRepository.java
+++ src/main/java/com/medium/HR/Tool/Backend/model/repositories/EmployeesRepository.java
@@ -1,6 +1,8 @@
 package com.medium.HR.Tool.Backend.model.repositories;
 
 import com.medium.HR.Tool.Backend.model.Employee;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 /**
@@ -8,4 +10,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
  * the JPA Repository for the employees
  */
 public interface EmployeesRepository extends JpaRepository<Employee, Integer> {
+    Page<Employee> findAll(Pageable pageable);
 }
