16bebe93f4947cad7cbddfc66a349b3b3e2e9eb3
Ansgar Nell <ansgar.nell@gmail.com>
2020-04-08 17:06:15 +0200

    Moved BasicDepartmentInfo projection into projections package.

--- src/main/java/com/medium/HR/Tool/Backend/model/projections/BasicDepartmentInfo.java
+++ src/main/java/com/medium/HR/Tool/Backend/model/projections/BasicDepartmentInfo.java
@@ -1,5 +1,6 @@
-package com.medium.HR.Tool.Backend.model;
+package com.medium.HR.Tool.Backend.model.projections;
 
+import com.medium.HR.Tool.Backend.model.Department;
 import org.springframework.data.rest.core.config.Projection;
 
 @Projection(
