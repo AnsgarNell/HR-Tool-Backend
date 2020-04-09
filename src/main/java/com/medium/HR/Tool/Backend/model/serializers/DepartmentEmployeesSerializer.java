8f14467d1700defb36d0df234fcb615efddda1d6
Ansgar Nell <ansgar.nell@gmail.com>
2020-04-09 21:10:57 +0200

    Removed calling a second serializer from within another serializer. Added TODO to check it later.

--- src/main/java/com/medium/HR/Tool/Backend/model/serializers/DepartmentEmployeesSerializer.java
+++ src/main/java/com/medium/HR/Tool/Backend/model/serializers/DepartmentEmployeesSerializer.java
@@ -4,21 +4,27 @@ import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.SerializerProvider;
 import com.medium.HR.Tool.Backend.model.DepartmentEmployee;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.medium.HR.Tool.Backend.model.Employee;
 
 import java.io.IOException;
 import java.util.List;
 
 public class DepartmentEmployeesSerializer extends JsonSerializer<List<DepartmentEmployee>> {
 
-    @Autowired
-    DepartmentEmployeeSerializer departmentEmployeeSerializer;
-
     @Override
     public void serialize(List<DepartmentEmployee> departmentEmployeeList, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) throws IOException {
         jsonGenerator.writeStartArray();
         for (DepartmentEmployee departmentEmployee : departmentEmployeeList) {
-            departmentEmployeeSerializer.serialize(departmentEmployee, jsonGenerator, serializerProvider);
+            // TODO: Try to somehow call the DepartmentEmployeeSerializer from here to
+            // avoid having duplicated code in both serializers
+            Employee employee = departmentEmployee.getEmployee();
+            jsonGenerator.writeStartObject();
+            jsonGenerator.writeStringField("empNo", employee.getEmpNo().toString());
+            jsonGenerator.writeStringField("firstName", employee.getFirstName());
+            jsonGenerator.writeStringField("lastName", employee.getLastName());
+            jsonGenerator.writeStringField("fromDate", departmentEmployee.getFromDate().toString());
+            jsonGenerator.writeStringField("toDate", departmentEmployee.getToDate().toString());
+            jsonGenerator.writeEndObject();
         }
         jsonGenerator.writeEndArray();
     }
--- src/main/java/com/medium/HR/Tool/Backend/model/serializers/EmployeeOfDepartmentsSerializer.java
+++ src/main/java/com/medium/HR/Tool/Backend/model/serializers/EmployeeOfDepartmentsSerializer.java
@@ -3,22 +3,26 @@ package com.medium.HR.Tool.Backend.model.serializers;
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.SerializerProvider;
+import com.medium.HR.Tool.Backend.model.Department;
 import com.medium.HR.Tool.Backend.model.DepartmentEmployee;
-import org.springframework.beans.factory.annotation.Autowired;
 
 import java.io.IOException;
 import java.util.List;
 
 public class EmployeeOfDepartmentsSerializer extends JsonSerializer<List<DepartmentEmployee>> {
-
-    @Autowired
-    EmployeeOfDepartmentSerializer employeeOfDepartmentSerializer;
-
     @Override
     public void serialize(List<DepartmentEmployee> departmentEmployeeList, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) throws IOException {
         jsonGenerator.writeStartArray();
-        for (DepartmentEmployee departmentEmployee : departmentEmployeeList) {
-            employeeOfDepartmentSerializer.serialize(departmentEmployee, jsonGenerator, serializerProvider);
+        for (DepartmentEmployee k : departmentEmployeeList) {
+            // TODO: Try to somehow call the EmployeeOfDepartmentSerializer from here to
+            // avoid having duplicated code in both serializers
+            Department department = k.getDepartment();
+            jsonGenerator.writeStartObject();
+            jsonGenerator.writeStringField("deptNo", department.getDeptNo());
+            jsonGenerator.writeStringField("deptName", department.getDeptName());
+            jsonGenerator.writeStringField("fromDate", k.getFromDate().toString());
+            jsonGenerator.writeStringField("toDate", k.getToDate().toString());
+            jsonGenerator.writeEndObject();
         }
         jsonGenerator.writeEndArray();
     }
