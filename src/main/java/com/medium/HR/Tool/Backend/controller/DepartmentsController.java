6ad43db24780e7bc003f8261af132d410d38669e
Ansgar Nell <ansgar.nell@gmail.com>
2020-04-09 19:24:09 +0200

    Implemented a temporary way of getting a paginated employees list for a department.

--- src/main/java/com/medium/HR/Tool/Backend/controller/DepartmentsController.java
+++ src/main/java/com/medium/HR/Tool/Backend/controller/DepartmentsController.java
@@ -1,21 +1,22 @@
 package com.medium.HR.Tool.Backend.controller;
 
 import com.medium.HR.Tool.Backend.model.Department;
-import com.medium.HR.Tool.Backend.model.Employee;
+import com.medium.HR.Tool.Backend.model.DepartmentEmployee;
+import com.medium.HR.Tool.Backend.model.projections.BasicDepartmentInfo;
+import com.medium.HR.Tool.Backend.model.repositories.DepartmentEmployeeRepository;
 import com.medium.HR.Tool.Backend.model.repositories.DepartmentsRepository;
-import com.medium.HR.Tool.Backend.model.repositories.EmployeesRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.*;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 /**
  * The DepartmentsController class contains the implementation of
@@ -27,6 +28,9 @@ public class DepartmentsController {
 
     @Autowired
     DepartmentsRepository departmentsRepository;
+
+    @Autowired
+    DepartmentEmployeeRepository departmentEmployeeRepository;
 
     /**
      * Lists all departments.
@@ -43,18 +47,28 @@ public class DepartmentsController {
      *
      * @param id This parameter contains the department identifier
      * @return ResponseEntity<Map<String,Object>> Returns the information in a map
-     * separating the department information and the managers information to avoid
-     * infinite JSON loops.
+     * separating the department information and the paginated employees information
      */
     @GetMapping("/{id}")
-    public ResponseEntity<Department> getDepartmentById(@PathVariable String id) {
-        Optional<Department> optionalDepartment = departmentsRepository.findById(id);
-
-        if(!optionalDepartment.isPresent()) {
+    public ResponseEntity<Map<String,Object>> getDepartmentById(
+            @PathVariable String id,
+            @RequestParam(value = "start", required = false, defaultValue = "0") Integer startOrNull,
+            @RequestParam(value = "limit", required = false, defaultValue = "10") Integer limitOrNull) {
+        Optional<BasicDepartmentInfo> optionalBasicDepartmentInfo = departmentsRepository.findByDeptNo(id);
+        if(!optionalBasicDepartmentInfo.isPresent()) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
         }
+        BasicDepartmentInfo basicDepartmentInfo = optionalBasicDepartmentInfo.get();
+        Map<String,Object> map = new HashMap<>();
+        map.put("Department", basicDepartmentInfo);
 
-        Department department = optionalDepartment.get();
-        return ResponseEntity.ok(department);
+        // TODO: Change the way the employees list is get, using somehow a pageable ManyToMany relationship.
+        Department department = new Department(basicDepartmentInfo.getDeptNo(), basicDepartmentInfo.getDeptName());
+        Pageable pageable = PageRequest.of(startOrNull, limitOrNull);
+        Page<DepartmentEmployee> departmentEmployeePage = departmentEmployeeRepository.findAllByDepartment(department, pageable);
+        List<DepartmentEmployee> departmentEmployeeList = departmentEmployeePage.toList();
+        map.put("Employees", departmentEmployeeList);
+
+        return ResponseEntity.ok(map);
     }
 }
