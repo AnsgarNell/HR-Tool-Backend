acf6cb2e802bb3e59896184bcded754387bf6591
Ansgar Nell <ansgar.nell@gmail.com>
2020-04-14 22:49:49 +0200

    Deleted unused query.

--- src/main/java/com/medium/HR/Tool/Backend/model/repositories/DepartmentEmployeeRepository.java
+++ src/main/java/com/medium/HR/Tool/Backend/model/repositories/DepartmentEmployeeRepository.java
@@ -3,14 +3,10 @@ package com.medium.HR.Tool.Backend.model.repositories;
 import com.medium.HR.Tool.Backend.model.Department;
 import com.medium.HR.Tool.Backend.model.DepartmentEmployee;
 import com.medium.HR.Tool.Backend.model.DepartmentEmployeeId;
-import com.medium.HR.Tool.Backend.model.Employee;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface DepartmentEmployeeRepository extends JpaRepository<DepartmentEmployee, DepartmentEmployeeId> {
     Page<DepartmentEmployee> findAllByDepartment(Department department, Pageable pageable);
-    List<DepartmentEmployee> findAllByEmployee(Employee employee);
 }
