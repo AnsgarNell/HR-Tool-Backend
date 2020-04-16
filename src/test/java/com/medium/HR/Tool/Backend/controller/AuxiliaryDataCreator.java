b8300147a68002694905e533aa50f81458aaa315
Ansgar Nell <ansgar.nell@gmail.com>
2020-04-16 10:59:47 +0200

    Created new functions to create data from a DepartmentDTO class.

--- src/test/java/com/medium/HR/Tool/Backend/controller/AuxiliaryDataCreator.java
+++ src/test/java/com/medium/HR/Tool/Backend/controller/AuxiliaryDataCreator.java
@@ -1,10 +1,12 @@
 package com.medium.HR.Tool.Backend.controller;
 
-import com.medium.HR.Tool.Backend.model.Department;
-import com.medium.HR.Tool.Backend.model.DepartmentManager;
-import com.medium.HR.Tool.Backend.model.Employee;
-import com.medium.HR.Tool.Backend.model.Gender;
+import com.medium.HR.Tool.Backend.model.*;
+import com.medium.HR.Tool.Backend.model.dtos.DepartmentDTO;
 import com.medium.HR.Tool.Backend.model.projections.DepartmentBasicInfo;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.projection.ProjectionFactory;
 import org.springframework.data.projection.SpelAwareProxyProjectionFactory;
 
@@ -28,9 +30,56 @@ public class AuxiliaryDataCreator {
         return employee;
     }
 
+    static public List<DepartmentEmployee> createDepartmentEmployees(Department department) {
+        List<DepartmentEmployee> departmentEmployeeList = new ArrayList<>();
+        for (int i = 1; i <= 10; i++) {
+            DepartmentEmployee departmentEmployee = new DepartmentEmployee();
+            departmentEmployee.setDepartment(department);
+            Employee  employee = new Employee();
+            employee.setEmpNo(i);
+            employee.setFirstName(String.format("TestFirsName%02d", i));
+            employee.setLastName(String.format("TestLastName %02d", i));
+            employee.setBirthDate(Date.valueOf(String.format("1980-%02d-01", i)));
+            employee.setGender((i%2 == 0 ? Gender.M : Gender.F));
+            employee.setHireDate(Date.valueOf(String.format("2000-%02d-01", i)));
+            departmentEmployee.setEmployee(employee);
+            departmentEmployee.setFromDate(Date.valueOf(String.format("2000-%02d-01", i)));
+            departmentEmployee.setToDate(Date.valueOf(String.format("9999-%02d-01", i)));
+            departmentEmployeeList.add(departmentEmployee);
+        }
+        return departmentEmployeeList;
+    }
+
+    static public List<DepartmentEmployee> createDepartmentEmployees(DepartmentDTO departmentDTO) {
+        return createDepartmentEmployees(departmentDTO.getDepartment());
+    }
+
+    static public Page<DepartmentEmployee> createPagedDepartmentEmployees(Department department) {
+        List<DepartmentEmployee> departmentEmployeeList = createDepartmentEmployees(department);
+        Pageable pageable = PageRequest.of(0, departmentEmployeeList.size());
+        Page<DepartmentEmployee> departmentEmployeePage = new PageImpl<DepartmentEmployee>(departmentEmployeeList, pageable, departmentEmployeeList.size());
+
+        return departmentEmployeePage;
+    }
+
+    static public Page<DepartmentEmployee> createPagedDepartmentEmployees(DepartmentDTO departmentDTO) {
+        List<DepartmentEmployee> departmentEmployeeList = createDepartmentEmployees(departmentDTO);
+        Pageable pageable = PageRequest.of(0, departmentEmployeeList.size());
+        Page<DepartmentEmployee> departmentEmployeePage = new PageImpl<DepartmentEmployee>(departmentEmployeeList, pageable, departmentEmployeeList.size());
+
+        return departmentEmployeePage;
+    }
+
     static public Department createDepartment() {
         Department department = new Department("d001", "Marketing");
         return department;
+    }
+
+    static public DepartmentDTO createDepartmentDTO() {
+        Department department = createDepartment();
+        DepartmentDTO departmentDTO = new DepartmentDTO();
+        departmentDTO.setDepartment(department);
+        return departmentDTO;
     }
 
     static public List<Department> createDepartmentList() {
@@ -67,5 +116,9 @@ public class AuxiliaryDataCreator {
 
         departmentManagerSet.add(departmentManager);
         department.setManagers(departmentManagerSet);
+    }
+
+    static public void createDepartmentManager(DepartmentDTO departmentDTO) {
+        createDepartmentManager(departmentDTO.getDepartment());
     }
 }
