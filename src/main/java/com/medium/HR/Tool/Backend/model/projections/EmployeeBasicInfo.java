91e34c326ca55375393f5daf418515e3f8258b68
Ansgar Nell <ansgar.nell@gmail.com>
2020-04-23 19:02:06 +0200

    Recovered EmployeeBasicInfo projection.

---
+++ src/main/java/com/medium/HR/Tool/Backend/model/projections/EmployeeBasicInfo.java
@@ -0,0 +1,8 @@
+package com.medium.HR.Tool.Backend.model.projections;
+
+public interface EmployeeBasicInfo {
+    Integer getEmpNo();
+    String getFirstName();
+    String getLastName();
+    String getCurrentTitle();
+}
