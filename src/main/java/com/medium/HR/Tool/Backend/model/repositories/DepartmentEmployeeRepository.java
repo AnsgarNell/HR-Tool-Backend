355b5faa9f353f6d586ceeed3b9d6f088eca8648
Ansgar Nell <ansgar.nell@gmail.com>
2020-04-23 18:18:24 +0200

    Use DepartmentEmployeeBasicInfo projection in DepartmentEmployeeRepository.

--- src/main/java/com/medium/HR/Tool/Backend/model/repositories/DepartmentEmployeeRepository.java
+++ src/main/java/com/medium/HR/Tool/Backend/model/repositories/DepartmentEmployeeRepository.java
@@ -3,6 +3,7 @@ package com.medium.HR.Tool.Backend.model.repositories;
 import com.medium.HR.Tool.Backend.model.Department;
 import com.medium.HR.Tool.Backend.model.DepartmentEmployee;
 import com.medium.HR.Tool.Backend.model.DepartmentEmployeeId;
+import com.medium.HR.Tool.Backend.model.projections.DepartmentEmployeeBasicInfo;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,5 +11,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface DepartmentEmployeeRepository extends JpaRepository<DepartmentEmployee, DepartmentEmployeeId> {
-    Optional<Page<DepartmentEmployee>> findAllByDepartment(Department department, Pageable pageable);
+    Optional<Page<DepartmentEmployeeBasicInfo>> findAllByDepartment(Department department, Pageable pageable);
 }
